111aec71971e41ce01d83cdc79bf892b6147adf4
David McInnis <davidm@eagles.ewu.edu>
2015-11-10 17:17:59 -0800

    added cleanup script

---
+++ example/clean.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Cleans up temporary created during latex process
+#
+###########################################################
+
+rm -f *.aux \
+      *.bbl \
+      *.blg \
+      *.loa \
+      *.lof \
+      *.log \
+      *.lot \
+      *.toc
